75771e801d236f164635637608dd8de68e371e0d
fergus <fergus.jingtai@gmail.com>
2020-02-19 16:12:17 +0800

    update

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 from flask import Flask
-# from flask import Flask, jsonify, request
-# from flask_cors import CORS
-# import numpy as np
-# import pandas as pd
-
-# import requests
-# from bs4 import BeautifulSoup
-
 app = Flask(__name__)
 
 @app.route('/')
