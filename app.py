099f0a14e4d78921d7987680ae14f4196a01f101
fergus <fergus.jingtai@gmail.com>
2020-03-16 16:09:48 +0800

    新增廣告數與評價

--- app.py
+++ app.py
@@ -15,24 +15,37 @@ def home():
 
 @app.route('/crawler_shopee')
 def shopee():
+    import json
+
     if 'keyword' in request.args:
         keyword = request.args['keyword']
     else:
         keyword = '筆記型電腦'
 
-    article_arr = crawler_shopee(keyword, 1)
-    # print('keyword = ', keyword)
-    print('article_arr = ', article_arr)
-    df = pd.DataFrame(article_arr, columns=['name', 'link', 'img', 'sales_volume', 'ad'])  # 使用 columns 調整排列順序
-    # print('df = ', df)
-    df['sales_volume'] = df['sales_volume'].astype('int')
-    df = df.sort_values(by='sales_volume', ascending=False)
-    df['key'] = range(1, len(df) + 1) # 增加 index 欄位
+    # keyword = '筆記型電腦'
+    shopee_product_ad = crawler_shopee_product_ad(keyword)
+    shopee_product_info = crawler_shopee_product_info(keyword)
 
-    return df.to_json(orient='records', force_ascii=False)
+    for i in range(len(shopee_product_ad)):
+        product_ad = shopee_product_ad.iloc[i]['name']
+        shopee_product_info.loc[shopee_product_info['name'] == product_ad, 'ad'] = True
+      
+    ad_num = len(shopee_product_ad)
+    print(keyword + '有 {} 個廣告'.format(ad_num))
+    shopee_product_info['sales_volume'] = shopee_product_info['sales_volume'].astype('int')
+    shopee_product_info = shopee_product_info.sort_values(by='sales_volume', ascending=False)
+    shopee_product_info['key'] = range(1, len(shopee_product_info) + 1) # 增加 index 欄位
+    shopee_product_info['ad_num'] = ad_num
+
+    return shopee_product_info.to_json(orient='records', force_ascii=False)
+    # return json.dumps([{
+    #     'ad_num': ad_num,
+    #     'data': shopee_product_info.to_json(orient='records', force_ascii=False)
+    # }])
     # return keyword
 
-def crawler_shopee(keyword, page):
+# 取得頁面html資料
+def fetch_page(keyword = '', page = 0):
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
     from selenium.webdriver.support.ui import WebDriverWait
@@ -44,6 +57,7 @@ def crawler_shopee(keyword, page):
     import pymysql.cursors
     import re
 
+    # 給 heroku 使用
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
@@ -53,6 +67,7 @@ def crawler_shopee(keyword, page):
     chrome_options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
+    # 本地開發使用
     # options = Options()
     # options.add_argument("window-size=1920,1080")
     # options.add_argument("--headless")  # 不要開啟瀏覽器
@@ -64,55 +79,109 @@ def crawler_shopee(keyword, page):
     # driver = webdriver.Chrome('./chromedriver',options=options)
     # driver.maximize_window()  # 瀏覽器視窗設為最大
 
-    try:
-        article_arr = []
+    url = 'https://shopee.tw/search?keyword={0}&page={1}&sortBy=sales'.format(keyword, page)
+    print('url = ', url)
+
+    driver.get(url)
+
+    # 等待選單內容出現
+    element = WebDriverWait(driver, 5).until(
+        expected_conditions.presence_of_element_located((By.CLASS_NAME, "shopee-search-item-result__item"))
+    )
+
+    # 頁面往下滑動
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+
+    # 取得內容
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    
+    driver.close()
+    return soup
         
-        for i in range(page):
-            url = 'https://shopee.tw/search?keyword={0}&page={1}&sortBy=sales'.format(keyword, i)
+# 從html取出商品array
+def get_article_arr(page_html):
+    article_arr = []
+    host = 'https://shopee.tw'
+    ad_articles = page_html.select('[data-sqe="ad"]')
+    ad_articles_len = len(ad_articles)
+
+    for article in ad_articles:
+        try:
+            article = article.parent.parent.parent.parent.parent.parent  # 回到 .shopee-search-item-result__item 那層
+            name = article.select('[data-sqe="name"]')[0].text
+            ad = True if len(article.select('[data-sqe="ad"]')) > 0 else False
+            article_arr.append({
+                'name': name,
+                'ad': ad
+            })
+        except Exception as e:
+            print(e)
+            print('---')
             
-            driver.get(url)
+    return article_arr
 
-            # 等待選單內容出現
-            element = WebDriverWait(driver, 5).until(
-                expected_conditions.presence_of_element_located((By.CLASS_NAME, "shopee-search-item-result__item"))
-            )
+# 設置爬取的關鍵字，及從第幾頁開始爬
+# 回傳商品 df
+def crawler_shopee_product_ad(keyword = '', page = 0):
+    is_fetch_next_page = True # 判斷是否繼續爬下一頁
+    df_article_total_arr = pd.DataFrame([])
+    
+    while is_fetch_next_page:
+        page_html = fetch_page(keyword, page)
+        article_arr = get_article_arr(page_html)
+        df_article_arr = pd.DataFrame(article_arr)
+        df_article_total_arr = pd.concat([df_article_total_arr, df_article_arr], ignore_index=True)
+        if len(df_article_arr) < 10:
+            is_fetch_next_page = False
+        page += 1
 
-            # 頁面往下滑動
-            scroll_height = 0
-            scroll_time = 7
-            # scroll_time = 3
-            for i in range(scroll_time):
-                scroll_height += 500
-                driver.execute_script('window.scrollTo(0, ' + str(scroll_height) + ');')
-                time.sleep(2)
+    return df_article_total_arr
 
-            # 取得內容
-            soup = BeautifulSoup(driver.page_source, 'lxml')
-            # print(soup)
-            host = 'https://shopee.tw'
-            articles = soup.select('.shopee-search-item-result__item')
-            for article in articles:
-                name = article.select('[data-sqe="name"]')[0].text
-                link = host + article.select('a')[0]['href']
-                img = article.select('a > div > div > img')[0]['src']
-                sales_volume = '0' if article.select('[data-sqe="rating"]')[0].next_sibling.text == '' else article.select('[data-sqe="rating"]')[0].next_sibling.text
-                sales_volume = re.findall(r'\d+', sales_volume)[0]
-                ad = True if len(article.select('[data-sqe="ad"]')) > 0 else False
+# 設置爬取的關鍵字，及從第幾頁開始爬
+# 回傳商品 df
+def crawler_shopee_product_info(keyword = '', page = 1):
+    import pandas as pd
+    import re
+    
+    article_arr = []
+    host = 'https://shopee.tw'
+    
+    for i in range(page):
+        url = 'https://shopee.tw/search?keyword={0}&page={1}&sortBy=sales'.format(keyword, i)
+        print(url)
+        headers = {
+            'user-agent': 'Googlebot'
+        }
+        resp = requests.get(url, headers=headers) 
+        soup = BeautifulSoup(resp.text, 'lxml')
 
-                article_arr.append({
-                    'name': name,
-                    'link': link,
-                    'img': img,
-                    'sales_volume': sales_volume,
-                    'ad': ad
-                })
+        articles = soup.select('.shopee-search-item-result__item')
+        for article in articles:
+            name = article.select('[data-sqe="name"]')[0].text
+            link = host + article.select('a')[0]['href']
+            img = article.select('a > div > div > img')[0]['src']
+            sales_volume = '0' if article.select('[data-sqe="rating"]')[0].next_sibling.text == '' else article.select('[data-sqe="rating"]')[0].next_sibling.text
+            sales_volume = re.findall(r'\d+', sales_volume)[0]
+            review = len(article.select('.shopee-rating-stars__stars .shopee-rating-stars__star-wrapper'))
+            price = article.select('[data-sqe="name"]')[0].next_sibling.text.replace('$', '').replace(',', '')
+            if len(price.split(' - ')) != 1:
+                price = (int(price.split(' - ')[0]) + int(price.split(' - ')[1])) / 2
+            monthly_revenue = float(sales_volume) * float(price)
 
-        return article_arr
-    except Exception as e:
-        print(e)
-    finally:
-        driver.close()
-        
+            article_arr.append({
+                'name': name,
+                'link': link,
+                'img': img,
+                'sales_volume': sales_volume,  # 月銷售量
+                'price': price,  # 單價
+                'monthly_revenue': monthly_revenue,  # 月收加總
+                'review': review,  # 評價
+                'ad': False
+            })
+
+    df = pd.DataFrame(article_arr, columns=['name', 'link', 'img', 'sales_volume', 'price', 'monthly_revenue', 'review', 'ad'])  # 使用 columns 調整排列順序
+    return df
 
 if __name__ == '__main__':
   app.config["DEBUG"] = True
